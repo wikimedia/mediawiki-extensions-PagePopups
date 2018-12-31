b51dd94262b289e5ad483aba82e78c2fd6d45652
Umherirrender <umherirrender_de.wp@web.de>
2018-12-31 16:02:09 +0100

    Add description message to be shown on Special:Version
    Text from https://www.mediawiki.org/wiki/Extension:PagePopups
    
    Activate banana checker for valid i18n files
    Change-Id: I23f7f898581012335e78495d940edbbc63b5dc31

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', /* 'banana' */ ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
